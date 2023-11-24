a86f4540bcfc5f4eeb301e16c303e772f62f290b
Joaooh98 <joaocarlosdepaiva1998@gmail.com>
2023-11-24 18:58:49 -0300

    fix:Exercise

---
+++ fundamentals/src/fundamentals/exercise/CalculatorIMC.java
@@ -0,0 +1,23 @@
+package fundamentals.exercise;
+
+import java.text.DecimalFormat;
+
+import javax.swing.JOptionPane;
+
+public class CalculatorIMC {
+    public static void main(String[] args) {
+
+        String height = JOptionPane.showInputDialog("QUAL A SUA ALTURA: ");
+        String Weight = JOptionPane.showInputDialog("QUAL A SEU PESO: ");
+
+        double numberTwo =Double.parseDouble(height);
+        double numberThree =Double.parseDouble(Weight);
+
+        double imc = numberThree / (numberTwo * numberTwo);
+
+        DecimalFormat format = new DecimalFormat("#.##");
+        String imcFormatado = format.format(imc);
+
+        JOptionPane.showMessageDialog(null, imcFormatado, "IMC", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
---
+++ fundamentals/src/fundamentals/exercise/Exercise.md
@@ -0,0 +1,11 @@
+    Criar um programa que leia a temperatura em Fahrenheit e converta para Celsius. ok 
+
+    Criar um programa que leia a temperatura em Celsius e converta para Fahrenheit. ok 
+
+    Criar um programa que leia o peso e a altura do usuário e imprima no console o IMC. ok
+
+    Criar um programa que leia um valor e apresente os resultados ao quadrado e ao cubo do valor. ok
+
+    Criar um programa que leia o valor da base e da altura de um triângulo e calcule a área. ok 
+
+    Criar um programa que resolve equações do segundo grau (ax2 + bx + c = 0) utilizando a fórmula de Bhaskara. Use como exemplo a = 1, b = 12 e c = -13. Encontre o delta ok 
