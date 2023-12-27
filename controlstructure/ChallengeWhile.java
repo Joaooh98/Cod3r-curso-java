deb41ee0ee247eef9321a442fb3e1a1905b15139
Joaooh98 <joaocarlosdepaiva1998@gmail.com>
2023-12-27 06:51:55 -0300

    desfio while e exemplo for

--- controlstructure/ChallengeWhile.java
+++ controlstructure/ChallengeWhile.java
@@ -6,13 +6,23 @@ public class ChallengeWhile {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int quantidadeNotar = 0;
-        double nota = 0;
+        int noteQuantity = 0;
+        double note = 0;
         double total = 0;
 
-        // while (nota !=) {
-            
-        // }
+        while (note != -1) {
+            System.out.println("informe  a nota (ou -1 p/sair): ");
+            note = input.nextDouble();
+            if (note <= 10 && note >= 0) {
+                total += note;
+                noteQuantity++;
+            } else {
+                System.out.println("nota invalida");
+            }
+            double average = total / noteQuantity;
+
+            System.out.println("media ="+average);
+        }
 
         input.close();
     }
---
+++ controlstructure/forExempleTwo.java
@@ -0,0 +1,9 @@
+package controlstructure;
+
+public class forExempleTwo {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 0; i -= 2) {
+            System.out.printf("contador = %d\n",i);
+        }
+    }
+}
