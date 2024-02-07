35d73f20a31117132ca7a983cf34ac4f0030335e
Joaooh98 <joaocarlosdepaiva1998@gmail.com>
2024-02-07 18:45:47 -0300

    128

---
+++ classes/Equalls.java
@@ -0,0 +1,21 @@
+package classes;
+
+public class Equalls {
+    public static void main(String[] args) {
+        User u1 = new User();
+        u1.name = "Pedro Silva";
+        u1.email = "pedro.silva@gmail.com.br";
+
+        User u2 = new User();
+        u2.name = "Pedro Silva";
+        u2.email = "pedro.silva@gmail.com.br";
+    
+        // sem implementar o equals no objeto User e retornado falso
+        System.out.println(u1 == u2);
+        System.out.println(u1.equals(u2)); // com equals implementado retorna true
+        System.out.println(u2.equals(u1)); // com equals implementado retorna true
+
+        System.out.println(u1.equals(new Data()));
+
+    }
+}
--- classes/User.java
+++ classes/User.java
@@ -1,5 +1,24 @@
 package classes;
 
 public class User {
+
+    String name;
+    String email;
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (obj instanceof User) {
+            
+            User userO = (User) obj;
+            
+            boolean nameEqual = userO.name.equals(this.name);
+            boolean emailEqual = userO.email.equals(this.email);
+            
+            return nameEqual && emailEqual;
+        }
+        return false;
+    }
+    
     
 }
